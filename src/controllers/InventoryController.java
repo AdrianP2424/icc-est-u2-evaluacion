3288382277c591a20e5d49c5a716ebf8525a8155
AdrianP2424 <guiacuencaapp@gmail.com>
2026-07-07 20:08:49 -0500

    Refactor InventoryController methods for filtering and grouping

--- src/controllers/InventoryController.java
+++ src/controllers/InventoryController.java
@@ -1,38 +1,64 @@
 package controllers;
 
+import java.util.List;
 import java.util.TreeSet;
-
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Comparator;
 import models.LabSupply;
 
 public class InventoryController {
     
-    public filterAndSortSupplies(List<LabSupply> supplies, int minimumStock; {
+    public TreeSet<LabSupply> filterAndSortSupplies(List<LabSupply> supplies, int minimumStock) {
+        
+        Comparator<LabSupply> comp = new Comparator<LabSupply>() {
+            @Override
+            public int compare(LabSupply s1, LabSupply s2) {
+                if (s1.getCodigo().equalsIgnoreCase(s2.getCodigo()) && s1.getStock() == s2.getStock()) {
+                    return 0;
+                }
+                if (s1.getStock() != s2.getStock()) {
+                    return Integer.compare(s2.getStock(), s1.getStock());
+                }
+                return s1.getNombre().compareToIgnoreCase(s2.getNombre());
+            }
+        };
 
-      public TreeSet<>
+        TreeSet<LabSupply> resultado = new TreeSet<>(comp);
 
+        for (LabSupply supply : supplies) {
+            if (supply.getStock() >= minimumStock) {
+                resultado.add(supply);
+            }
+        }
 
-
-
-
-
-
+        return resultado;
     }
     
-    public groupCodeByStock(List<LabSupply supplies>){
+    public Map<String, Set<Integer>> groupCodesByStock(List<LabSupply> supplies) {
+        Map<String, Set<Integer>> agruparCodigos = new TreeMap<>();
+        
+        agruparCodigos.put("ALTA", new TreeSet<>());
+        agruparCodigos.put("MEDIA", new TreeSet<>());
+        agruparCodigos.put("BAJA", new TreeSet<>());
 
+        for (LabSupply supply : supplies) {
+            String numeroStr = supply.getCodigo().substring(3);
+            int numero = Integer.parseInt(numeroStr);
 
-
-
+            if (supply.getStock() >= 4) {
+                agruparCodigos.get("ALTA").add(numero);
+            } else if (supply.getStock() <= 3 && supply.getStock() >= 2) {
+                agruparCodigos.get("MEDIA").add(numero);
+            } else if (supply.getStock() == 1) {
+                agruparCodigos.get("BAJA").add(numero);
+            }
+        }
+        
+        return agruparCodigos;
     }
-    
-    
-    
-    
-    
-
-
-
-
-
-
 }
+
+
+
